0d68ba7033b5ca54bfcc287e99489910dac8bd71
John Nobile <2840038+jnobile@users.noreply.github.com>
2022-12-02 04:23:10 +0000

    Add solution and tests for Day 1

---
+++ day-01/elfCalories.js
@@ -0,0 +1,34 @@
+export function topElvesCalories(input) {
+  let maxLoad = -1;
+  let currentElfLoad = 0;
+  let trimmedInput = input.split('\n').map((x) => x.trim())
+  for (let i = 0; i <= trimmedInput.length; i++) {
+    let currentItem = trimmedInput[i];
+    if (currentItem === '') {
+      if (currentElfLoad > maxLoad) {
+        maxLoad = currentElfLoad;
+      }
+      currentElfLoad = 0;
+    } else {
+      currentElfLoad += parseInt(currentItem);
+    }
+  }
+  return maxLoad;
+};
+
+export function topThreeElvesCalories(input) {
+  let maxLoads = [];
+  let currentElfLoad = 0;
+  let trimmedInput = input.split('\n').map((x) => x.trim())
+  for (let i = 0; i <= trimmedInput.length; i++) {
+    let currentItem = trimmedInput[i];
+    if (currentItem === '') {
+      maxLoads.push(currentElfLoad)
+      currentElfLoad = 0;
+    } else {
+      currentElfLoad += parseInt(currentItem);
+    }
+  }
+  return maxLoads.sort().reverse().slice(0,3).reduce((a, b) => a + b);
+};
+
